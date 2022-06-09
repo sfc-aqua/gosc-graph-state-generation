6fea4b5f97cf7c1a5cbcfc2c9e7bd566c6dab015
liusitong1011 <liusitong@keio.jp>
2022-06-10 08:12:19 +0900

    fixed the error when ordering the stablizers

--- 2-tile-patch-ver.py
+++ 2-tile-patch-ver.py
@@ -2,19 +2,23 @@
 #no ancilla so far
 
 import numpy
-import heapq
+#import heapq
+import random
+from create_random_graph import gen_erdos_renyi_graph_single_component
+import find_order_stablizers as order
+import warnings
+import matplotlib.cbook
+warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
 
-time_stamp=0 #for gosc rules time step, not used for now
-#the value for board, row and col here is just for understanding
-num_row=3
-num_col=8
-
+"""
 adj_list={
     "0":["1"],
     "1":["0","2"],
     "2":["1","3"],
     "3":["2"]
 }
+"""
+time_stamp=0 #for gosc rules time step, not used for now
 
 def read_adjacency(ad_list):
     # Read adjacency list as input in dictionary form
@@ -59,22 +63,30 @@ def schedule_stablizers(operations):
     heap=[]
     operation_steps=[]
     n = len(operations)
-    step=0
+    #step=0
 
     sorted_start=sorted(operations, key=lambda x: min(x))
     sorted_stablizers=create_stablizer(sorted_start)
-    heapq.heappush(heap,max(sorted_start[0]))
-    operation_steps.append([sorted_stablizers[0]])
+
+    #wrong
+    #heapq.heappush(heap,max(sorted_start[0]))
+    #operation_steps.append([sorted_stablizers[0]])
+    order.heappush(heap, max(sorted_start[0]),operation_steps,[sorted_stablizers[0]])
 
     for i in range(1,n):
         if min(sorted_start[i])>heap[0]:
-            heapq.heappop(heap)
-            operation_steps[0].append(sorted_stablizers[i])
-            operation_steps.append(operation_steps.pop(0))
+            print('bingo!!!!!')
+            order.heappop(heap,operation_steps)
+            sorted_stablizers[i]=[sorted_stablizers[0],sorted_stablizers[i]]
+            order.heappush(heap, max(sorted_start[i]), operation_steps, sorted_stablizers[i])
+            #wrong
+            #operation_steps[0].append(sorted_stablizers[i])
+            #operation_steps.append(operation_steps.pop(0))
         else:
-            step+=1
-            operation_steps.append([sorted_stablizers[i]])
-        heapq.heappush(heap,max(sorted_start[i]))
+            #step+=1
+            #wrong
+            #operation_steps.append([sorted_stablizers[i]])
+            order.heappush(heap,max(sorted_start[i]),operation_steps,[sorted_stablizers[i]])
     return heap,operation_steps
 
 def map_to_device(qubits_num,qubits_index):
@@ -106,18 +118,26 @@ def stablizer_to_device(operation_steps,pairs_id_index):
                         stablizer_on_device[i][pairs_id_index[k]].append(operation_steps[i][j][k])
     return stablizer_on_device
 
+
 #run the compilation
+n_nodes_ran=random.randint(3, 9)
+n_edges_ran=random.randint(n_nodes_ran, n_nodes_ran+3)
+G,adj_list_gen=gen_erdos_renyi_graph_single_component(n_nodes_ran,n_edges_ran)
+#the value for board, row and col here is just for understanding
+num_row=3
+num_col=2*n_nodes_ran
 board=create_board(num_row,num_col)
-num_nodes,node_adj=read_adjacency(adj_list)
-num_step, operation_steps=schedule_stablizers(node_adj)
+num_nodes,node_adj=read_adjacency(adj_list_gen)
 patch_index=create_qubits_index(num_col)
 pairs_id_index, board, num_patch=map_to_device(num_nodes,patch_index)
+num_step, operation_steps=schedule_stablizers(node_adj)
 stablizer_on_qubits=stablizer_to_device(operation_steps,pairs_id_index)
 
 
 #print out the results
+print("adjacency list:"+str(adj_list_gen))
 print("number of step to implement stablizer: "+str(len(num_step)))
-print("order of apply stablizers: "+str(operation_steps))
+print("order of implementing stablizers: "+str(operation_steps))
 print("measurement on qubits to apply: "+str(stablizer_on_qubits))
 #print("number of patch:"+str(num_patch))
-print(board)
+print("qubit allocation on device:\n"+str(board))
---
+++ find_order_stablizers.py
@@ -0,0 +1,60 @@
+#source code of heap
+def heappush(heap, item,stablizer_order,stablizer):
+    """Push item onto heap, maintaining the heap invariant."""
+    heap.append(item)
+    stablizer_order.append(stablizer)
+    _siftdown(heap, 0, len(heap)-1,stablizer_order)
+
+def _siftdown(heap, startpos, pos, stablizer_order):
+    newitem = heap[pos]
+    newstablizer = stablizer_order[pos]
+    # Follow the path to the root, moving parents down until finding a place
+    # newitem fits.
+    while pos > startpos:
+        parentpos = (pos - 1) >> 1
+        parent = heap[parentpos]
+        parent_stablizer = stablizer_order[parentpos]
+        if newitem < parent:
+            heap[pos] = parent
+            stablizer_order[pos]=parent_stablizer
+            pos = parentpos
+            continue
+        break
+    heap[pos] = newitem
+    stablizer_order[pos] = newstablizer
+
+def heappop(heap,stablizer_order):
+    """Pop the smallest item off the heap, maintaining the heap invariant."""
+    lastelt = heap.pop()    # raises appropriate IndexError if heap is empty
+    laststa = stablizer_order.pop()
+    if heap:
+        returnitem = heap[0]
+        returnsta = stablizer_order[0]
+        heap[0] = lastelt
+        stablizer_order[0] = laststa
+        _siftup(heap, 0,stablizer_order)
+        return returnitem,returnsta
+    return lastelt,laststa
+
+def _siftup(heap, pos,stablizer_order):
+    endpos = len(heap)
+    startpos = pos
+    newitem = heap[pos]
+    newsta = stablizer_order[pos]
+    # Bubble up the smaller child until hitting a leaf.
+    childpos = 2*pos + 1    # leftmost child position
+    while childpos < endpos:
+        # Set childpos to index of smaller child.
+        rightpos = childpos + 1
+        if rightpos < endpos and not heap[childpos] < heap[rightpos]:
+            childpos = rightpos
+        # Move the smaller child up.
+        heap[pos] = heap[childpos]
+        stablizer_order[pos] = stablizer_order[childpos]
+        pos = childpos
+        childpos = 2*pos + 1
+    # The leaf at pos is empty now.  Put newitem there, and bubble it up
+    # to its final resting place (by sifting its parents down).
+    heap[pos] = newitem
+    stablizer_order[pos] = newsta
+    _siftdown(heap, startpos, pos, stablizer_order)
