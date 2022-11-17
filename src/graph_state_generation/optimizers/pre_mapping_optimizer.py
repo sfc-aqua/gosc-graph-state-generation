0434589b66e38f782d8ebcc3cc20cf09a61d5aac
AthenaCaesura <athena.caesura@zapatacomputing.com>
2022-11-16 19:35:46 -0500

    feat: changes from meeting

--- src/graph_state_generation/optimizers/pre_mapping_optimizer.py
+++ src/graph_state_generation/optimizers/pre_mapping_optimizer.py
@@ -6,7 +6,8 @@ import numpy as np
 
 def approximate_static_stabilizer_reduction(g: nx.Graph) -> Tuple[Set[int], nx.Graph]:
     """Uses maximum independent set algorithm to find state initialization such that
-    most stabilizer generators are already stabilized so the stabilizer measurements needed are reduced."""
+    most stabilizer generators are already stabilized so the stabilizer measurements
+    needed are reduced."""
     max_i_set = nx.algorithms.approximation.maximum_independent_set(g)
     return (max_i_set, g)
 
