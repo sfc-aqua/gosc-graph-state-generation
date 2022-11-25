fbc1aef251909f5fdc7e88402073c24b43501b3d
liusitong1011 <liusitong@keio.jp>
2022-11-25 10:01:19 +0900

    Fixed random seed in gen_graph; changed import path in tests

--- data/gen_graphs.py
+++ data/gen_graphs.py
@@ -1,6 +1,8 @@
 import json
 import random
-import subprocess
+import os
+
+import math
 
 import networkx as nx
 
@@ -8,7 +10,7 @@ import networkx as nx
 def gen_erdos_renyi_graph_single_component(n, m):
     if_connected = 0
     for i in range(1000):
-        G = nx.gnm_random_graph(n, m)
+        G = nx.gnm_random_graph(n, m, seed=51)
         if_connected = nx.number_connected_components(G)
         if if_connected == 1:
             break
@@ -24,17 +26,23 @@ for i in range(5):
         # maximal number of edges for undirected connected graph is n(n-1)/2 edges
         edge_numbers = n_nodes_ran * (n_nodes_ran - 1) / 2 - n_nodes_ran
 
+        random.seed(10)
+
         n_edges_very_sparse = random.randint(
-            n_nodes_ran, n_nodes_ran + edge_numbers / 4
+            n_nodes_ran, math.floor(n_nodes_ran + edge_numbers / 4)
         )
+
         n_edges_sparse = random.randint(
-            n_nodes_ran + edge_numbers / 4, n_nodes_ran + edge_numbers / 2
+            math.floor(n_nodes_ran + edge_numbers / 4),
+            math.floor(n_nodes_ran + edge_numbers / 2),
         )
         n_edges_dense = random.randint(
-            n_nodes_ran + edge_numbers / 2, n_nodes_ran + edge_numbers * 3 / 4
+            math.floor(n_nodes_ran + edge_numbers / 2),
+            math.floor(n_nodes_ran + edge_numbers * 3 / 4),
         )
         n_edges_very_dense = random.randint(
-            n_nodes_ran + edge_numbers * 3 / 4, n_nodes_ran + edge_numbers
+            math.floor(n_nodes_ran + edge_numbers * 3 / 4),
+            math.floor(n_nodes_ran + edge_numbers),
         )
 
         very_sparse = gen_erdos_renyi_graph_single_component(
@@ -45,59 +53,13 @@ for i in range(5):
         very_dense = gen_erdos_renyi_graph_single_component(
             n_nodes_ran, n_edges_very_dense
         )
-
-        with open(f"graphs/very_sparse_{i}.json", "w") as json_file:
+        if not os.path.exists("graph"):
+            os.makedirs("graph")
+        with open(f"graph/very_sparse_{i}.json", "w") as json_file:
             json.dump(very_sparse, json_file)
-        with open(f"graphs/dense_{i}.json", "w") as json_file:
+        with open(f"graph/dense_{i}.json", "w") as json_file:
             json.dump(sparse, json_file)
-        with open(f"graphs/dense_{i}.json", "w") as json_file:
+        with open(f"graph/dense_{i}.json", "w") as json_file:
             json.dump(dense, json_file)
-        with open(f"graphs/very_dense_{i}.json", "w") as json_file:
+        with open(f"graph/very_dense_{i}.json", "w") as json_file:
             json.dump(very_dense, json_file)
-
-
-#     very_sparse_graphs = open("very_sparse_graphs.txt", "a", encoding="utf-8")
-#     sparse_graphs = open("sparse_graphs.txt", "a", encoding="utf-8")
-#     dense_graphs = open("dense_graphs.txt", "a", encoding="utf-8")
-#     very_dense_graphs = open("very_dense_graphs.txt", "a", encoding="utf-8")
-
-#     if (
-#         very_sparse is not None
-#         and int(subprocess.getoutput("wc -l %s" % "very_sparse_graphs.txt").split()[0])
-#         < 100
-#     ):
-#         print("%s" % (very_sparse), file=very_sparse_graphs)
-#     if (
-#         sparse is not None
-#         and int(subprocess.getoutput("wc -l %s" % "sparse_graphs.txt").split()[0]) < 100
-#     ):
-#         print("%s" % (sparse), file=sparse_graphs)
-#     if (
-#         dense is not None
-#         and int(subprocess.getoutput("wc -l %s" % "dense_graphs.txt").split()[0]) < 100
-#     ):
-#         print("%s" % (dense), file=dense_graphs)
-#     if (
-#         very_dense is not None
-#         and int(subprocess.getoutput("wc -l %s" % "very_dense_graphs.txt").split()[0])
-#         < 100
-#     ):
-#         print("%s" % (very_dense), file=very_dense_graphs)
-
-
-# print(
-#     "Very sparse graph has %s lines"
-#     % subprocess.getoutput("wc -l %s" % "very_sparse_graphs.txt").split()[0]
-# )
-# print(
-#     "Sparse graph has %s lines"
-#     % subprocess.getoutput("wc -l %s" % "sparse_graphs.txt").split()[0]
-# )
-# print(
-#     "Dense graph has %s lines"
-#     % subprocess.getoutput("wc -l %s" % "dense_graphs.txt").split()[0]
-# )
-# print(
-#     "Very dense graph has %s lines"
-#     % subprocess.getoutput("wc -l %s" % "very_dense_graphs.txt").split()[0]
-# )
