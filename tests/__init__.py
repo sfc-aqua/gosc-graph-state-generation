038ef9a2e287b042543a61609fad6a10843839fb
whit3z <whit3z@sfc.wide.ad.jp>
2022-11-25 18:00:34 +0900

    comply to importlib mode of pytest

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-from .test_pre_mapping_optimizer import (
-    test_max_independet_set_reduction_0,
-    test_max_independet_set_reduction_1,
-)
-from .test_stabilizer_measurement_scheduler import (
-    test_greedy_scheduler_0,
-    test_greedy_scheduler_1,
-)
