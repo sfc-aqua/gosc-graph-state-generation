5a8240d1da9ccd826ebc3abf13b6d5fd25291306
YinjieZhou <57865556+YinjieZhou@users.noreply.github.com>
2023-07-07 13:23:12 +0900

    Update node_to_patch_mapper.py

--- src/graph_state_generation/optimizers/node_to_patch_mapper.py
+++ src/graph_state_generation/optimizers/node_to_patch_mapper.py
@@ -5,7 +5,7 @@ def total_edge_weight(g, layout):
     total_weight = 0
     for edge in g.edges(data=False):
         # Weight of an edge is the number of nodes it travels over
-        weight = abs(layout.index(edge[0]) - layout.index(edge[1]))
+        weight = abs(layout.index(edge[0]) - layout.index(edge[1])) - 1
         total_weight += weight
     return total_weight
 
@@ -15,19 +15,23 @@ def NGAopt_layout(g: nx.Graph, _: Set[int]) -> List[int]:
     best_layout = nodes[:]
     best_weight = total_edge_weight(g, best_layout)
     
-    for i in range(1, n):
-        
-        # (3) Swap current with the node to its left
-        nodes[i], nodes[i - 1] = nodes[i - 1], nodes[i]
-        
-        # (2) Calculate the total edge weight
-        new_weight = total_edge_weight(g, nodes)
-        
-        # (4) If the new total edge weight is higher or the same, undo the swap
-        if new_weight >= best_weight:
+    while True:
+        weight_change = False
+        for i in range(1, n):
+
+            # (3) Swap current with the node to its left
             nodes[i], nodes[i - 1] = nodes[i - 1], nodes[i]
-        else:
-            best_weight = new_weight
-            best_layout = nodes[:]
-    
+
+            # (2) Calculate the total edge weight
+            new_weight = total_edge_weight(g, nodes)
+
+            # (4) If the new total edge weight is higher or the same, undo the swap
+            if new_weight >= best_weight:
+                nodes[i], nodes[i - 1] = nodes[i - 1], nodes[i]
+            else:
+                best_weight = new_weight
+                best_layout = nodes[:]
+                weight_change = True
+        if not weight_change:
+            break
     return best_layout
